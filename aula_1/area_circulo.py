74db89d90cb0aa6fa641e180a53c0295ea6e57ef
Luan Teylo <luanteylo@gmail.com>
2019-02-10 18:14:13 -0200

    first commit

---
+++ aula_1/area_circulo.py
@@ -0,0 +1,8 @@
+raio = float(input())
+
+pi = 3.14 
+
+area = pi*(raio**2)
+
+
+print("%.3f" % area)
---
+++ aula_1/hello_world.py
@@ -0,0 +1,7 @@
+print("Ola!")
+# isso eh um comentario e nao será executado 
+nome = input("Qual o seu nome?\n")
+
+print("Olá " + nome + " bem vindo ao curso de python!")
+
+
